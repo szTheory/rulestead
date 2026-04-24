aded154c8e6232e69d28a3d0a14a6c3ad16d4913
szTheory <szTheory@users.noreply.github.com>
2026-04-23 21:08:40 -0400

    feat(05-03): default tests to fake-first harness
    - start Rulestead.Fake in test_helper and set it as the default store
    - preserve manual Ecto sandbox setup for repo-backed tests

--- rulestead/test/test_helper.exs
+++ rulestead/test/test_helper.exs
@@ -2,5 +2,8 @@ ExUnit.start()
 
 {:ok, _} = Application.ensure_all_started(:ecto_sql)
 {:ok, _} = Rulestead.Repo.start_link()
+Application.put_env(:rulestead, :store, Rulestead.Fake)
+Rulestead.Fake.Control.ensure_started()
+Rulestead.Fake.Control.reset!()
 
 Ecto.Adapters.SQL.Sandbox.mode(Rulestead.Repo, :manual)
